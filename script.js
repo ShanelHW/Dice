04b0ec11fcb0b373a5b669dcaf2daa1ef0a37567
Shanel Hickman-Davis <shanelhickmandavis@gmail.com>
2022-09-15 14:27:45 -0400

    game logic update
    css changes
    create multiround scoring
    reset displays a pop-up of final winner

--- script.js
+++ script.js
@@ -10,34 +10,71 @@
 // leaving just the string "PLAYER WINS" or "COMPUTER WINS" and save it to a variable (winner)
 // FINALLY: return all three variables using an array
 // return [VALUE1, VALUE2, VALUE3]
-let gameInfo;
+let results;
 let playerScore = 0;
 let computerScore = 0;
 let plays = 0;
-    const rollDice = () =>{
-    let playerDice = Math.ceil(Math.random() * 6);
-    let computerDice = Math.ceil(Math.random() * 6);
-    //const winner = playerDice > computerDice ? `PLAYER WINS` : 'COMPUTER WINS'
-    const winner = playerDice == computerDice ? `TIE` : (playerDice > computerDice) ? 'PLAYER WINS' : 'COMPUTER WINS';
-    gameInfo = [playerDice, computerDice, winner];
-    playerDice > computerDice ? playerScore++ : computerScore++
-    plays++;
-    console.log(plays)
-    return gameInfo;
-    }
-//console.log(rollDice());
+const winnerDiv = document.querySelector("#winner")
+let runningScore = document.querySelector('#running-score')
+const rollDice = () => {
+  let playerDice = Math.ceil(Math.random() * 6);
+  let computerDice = Math.ceil(Math.random() * 6);
+  //const winner = playerDice > computerDice ? `PLAYER WINS` : 'COMPUTER WINS'
+  const winner =
+    playerDice == computerDice
+      ? /*If tied*/ `It's a Tie`
+      : playerDice > computerDice
+      ? /*If player wins*/ "PLAYER wins "
+      : /*If computer wins*/ "COMPUTER wins ";
+  results = [playerDice, computerDice, winner];
+  playerDice > computerDice ? playerScore++ : computerScore++;
+  plays++;
+  console.log(`number of rounds: ${plays} Computer:${computerDice} Player:${playerDice} Winner: ${winner}`);
+  return results;
+};
+console.log(rollDice());
 // ============ Task 2 ==============================
 // Let's update our UI (User Interface)
 // Create a function (displayScore)
-    const playBtn = document.querySelector('#play-game')
-    playBtn.addEventListener('click', rollDice)
-    const resetBtn = document.querySelector(`#reset`)
-    resetBtn.addEventListener('click', ()=>{ console.log("RESET"); computerScore= 0; playerScore=0; plays= 0})
-    const displayScore = () =>{
-        var newScore = document.createElement('div');
-        newScore.innerText(`Player: ${playerScore} Computer: ${computerScore}`)
-    }
 
+
+const reset = () =>  {
+const finalWinner = playerScore == computerScore ? 'GAME IS A TIE!' : (playerScore>computerScore ? `PLAYER WINS` : `COMPUTER WINS`)
+window.alert(finalWinner)
+console.log("RESET");
+computerScore = 0;
+playerScore = 0;
+plays = 0;
+//remove all rounds of game
+while(document.querySelector('#winner').firstElementChild){
+    document.querySelector('#winner').firstElementChild.remove()}
+    //reset score
+document.querySelector('#running-score').firstChild.textContent(`Player:      Computer:      Winner:    `)
+}
+const displayScore = () => {
+  
+  const div = document.createElement(`div`);
+  div.textContent = 
+  `Player: ${results[0]} 
+  Computer: ${results[1]}
+  Winner: ${results[2]} round ${plays}`
+  winnerDiv.appendChild(div)
+  document.querySelector('#running-score').firstChild.textContent = `Player: ${playerScore} Computer: ${computerScore} `
+  console.log(div);
+};
+
+
+
+const playGame = () => {
+    rollDice();
+    displayScore();
+}
+
+const playBtn = document.querySelector("#play-game");
+playBtn.addEventListener("click", playGame);
+
+const resetBtn = document.querySelector(`#reset`);
+resetBtn.addEventListener("click", reset);
 // Create a new a div displaying:
 // Computer Score: , Player Score:  & the Winner:
 // so there is a running record of game data.
